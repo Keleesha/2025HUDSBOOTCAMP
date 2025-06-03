cfcf3696ceda1f2dea10997a117480142dee33a2
Keleesha <keleeshalowe1@gmail.com>
2025-06-03 13:22:22 -0500

    Add files via upload
    simple alternating "x" and "o" matrix using nested loops

---
+++ matrix.py
@@ -0,0 +1,8 @@
+for row in range(8):
+    for column in range (8): 
+        if column % 2 ==0:
+            print("o",end="")
+        else: 
+            print("x",end="")    
+
+    print ("")    
